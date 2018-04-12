3ca8ccbe94fb7a166bc34ea1a6b0ed15131febac
zhyrao <rain330047@outlook.com>
2018-04-12 15:17:49 +0800

    decarotor pattern

---
+++ Chapter_Three/Beverage.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_Three
+{
+    // 原始设计的Beverage类 抽象类
+    public abstract class Beverage
+    {
+        public string description = "Unkown Beverage";
+
+        // 已经实现
+        public string getDescription()
+        {
+            return description;
+        }
+
+        // 必须在子类实现
+        public abstract double cost();
+    }
+
+    // 装饰者类
+    // 首先要继承Beverage类，使得有同样的类型
+    public abstract class CondimentDecorator: Beverage
+    {
+        public abstract new string getDescription();
+    }
+
+
+    // 组件类
+    public class Expresso : Beverage
+    {
+        public Expresso()
+        {
+            description = "Espresso";
+        }
+
+        public override double cost()
+        {
+            return 1.99;
+        }
+    }
+
+    // 组件类
+    public class HouseBlend:Beverage
+    {
+        public HouseBlend()
+        {
+            description = "House Blend Coffee";
+        }
+
+        public override double cost()
+        {
+            return 0.99;
+        }
+    }
+
+    // 组件类
+    public class DarkRoast:Beverage
+    {
+        public DarkRoast()
+        {
+            description = "DarkRoast Coffee";
+        }
+
+        public override double cost()
+        {
+            return 1.59;
+        }
+    }
+
+    // 组件类
+    public class Decat:Beverage
+    {
+        public Decat()
+        {
+            description = "Decat Coffee";
+        }
+
+        public override double cost()
+        {
+            return 1.79;
+        }
+    }
+
+    // 装饰者类
+    public class Mocha : CondimentDecorator
+    {
+        Beverage beverage;
+
+        public Mocha(Beverage b)
+        {
+            this.beverage = b;
+        }
+
+        public override string getDescription()
+        {
+            return beverage.getDescription() + ", Mocha";
+        }
+
+        public override double cost()
+        {
+            return beverage.cost() + 0.20;
+        }
+    }
+
+    public class Soy :CondimentDecorator
+    {
+        Beverage beverage;
+        public Soy(Beverage b)
+        {
+            this.beverage = b;
+        }
+
+        public override string getDescription()
+        {
+            return beverage.getDescription() + ", Soy";
+        }
+
+        public override double cost()
+        {
+            return 0.15 + beverage.cost();
+        }
+    }
+
+    public class Whip:CondimentDecorator
+    {
+        Beverage beverage;
+
+        public Whip(Beverage b)
+        {
+            this.beverage = b;
+        }
+
+        public override string getDescription()
+        {
+            return beverage.getDescription() + ", Whip";
+        }
+
+        public override double cost()
+        {
+            return 0.25 + beverage.cost();
+        }
+    }
+}
---
+++ Chapter_Three/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_Three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Beverage espressor = new Expresso();
+            Console.WriteLine(espressor.getDescription() + " $ " + espressor.cost());
+
+            Beverage beverage2 = new DarkRoast();
+            Console.WriteLine(beverage2.getDescription() + " $ " + beverage2.cost());
+            beverage2 = new Mocha(beverage2);
+            beverage2 = new Soy(beverage2);
+            beverage2 = new Whip(beverage2);
+
+            Console.WriteLine(beverage2.getDescription() + " $ " + beverage2.cost());
+
+            Beverage beverage3 = new HouseBlend();
+
+            beverage3 = new Mocha(beverage3);
+            beverage3 = new Soy(beverage3);
+            beverage3 = new Whip(beverage3);
+
+            Console.WriteLine(beverage3.getDescription() + " $ " + beverage3.cost());
+
+            Console.ReadKey();
+        }
+    }
+}
