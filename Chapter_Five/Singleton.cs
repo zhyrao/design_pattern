c219088376d28e2e19087521baf7e2273a42f12d
zhyrao <rain330047@outlook.com>
2018-04-13 09:57:26 +0800

    singleton pattern

---
+++ Chapter_Five/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_Five
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        private Singleton() { }
+        public static Singleton getInstance()
+        {
+            if (instance == null)
+                instance = new Singleton();
+
+            return instance;
+        }
+    }
+}
