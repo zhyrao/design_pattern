e948ff2a5fbdaa1cc751e70907044bf774e3c9cc
zhyrao <rain330047@outlook.com>
2018-04-13 09:56:46 +0800

    Factory pattern

---
+++ Chapter_Four/Materials.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_Four
+{
+    class Materials
+    {
+    }
+
+    // 面团原料
+    public class Dough
+    {
+        public Dough()
+        {
+
+        }
+    }
+
+    // 酱料原料
+    public class Sauce
+    {
+        public Sauce()
+        {
+
+        }
+    }
+
+    // 芝士原料
+    public class Cheese
+    {
+        public Cheese()
+        { }
+    }
+
+    public class Veggies
+    {
+        public Veggies() { }
+    }
+
+    public class Pepperoni
+    {
+        public Pepperoni() { }
+    }
+
+    public class Clam
+    {
+        public Clam() { }
+    }
+}
---
+++ Chapter_Four/PizzaIngredientFactory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_Four
+{
+    public interface PizzaIngredientFactory
+    {
+        Dough createDough();
+        Sauce createSauce();
+        Cheese createCheese();
+        Veggies[] createVeggies();
+        Pepperoni createPepperoni();
+        Clam createClam();
+    }
+
+    public class NYPizzaIngredientFactory : PizzaIngredientFactory
+    {
+        public Cheese createCheese()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Clam createClam()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dough createDough()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pepperoni createPepperoni()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Sauce createSauce()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Veggies[] createVeggies()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
