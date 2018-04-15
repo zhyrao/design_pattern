46c1f667806120f982f908258cde5656d22387d8
rain330047@outlook.com <rain330047@outlook.com>
2018-04-15 23:56:08 +0800

    Coffee example

---
+++ Chapter_Eight/Coffee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_Eight
+{
+    public class Coffee
+    {
+        public void prepareRecipte()
+        {
+
+        }
+
+        public void boilWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+
+        public void brewCoffeeGrinds()
+        {
+            Console.WriteLine("Dripping Coffee through filter");
+        }
+
+        public void pourInCup()
+        {
+            Console.WriteLine("Pouring into cup");
+        }
+
+        public void addSugarAndMilk()
+        {
+            Console.WriteLine("Adding sugar and milk");
+        }
+    }
+}
